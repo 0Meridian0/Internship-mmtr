ff8b5546f83939a669b16a7cebc1140617b2acdd
Alexander Smirnov <aasmirnov@mmtr.ru>
2022-12-05 12:17:04 +0300

    Добавил модуль обработки входящих запросов, который создает отдельный токен и использует его в укороченной ссылке.

---
+++ UrlCutter/UrlCutter/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UrlCutter.Models;
+
+namespace UrlCutter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult UrlCutter(string id)
+        {
+            if (id != null)
+            {
+                URL url = new URL(id);
+
+                return Json(url.ToString());
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UrlCutter(URL personUrl) 
+        {
+            if (personUrl != null)
+            {
+                URL url = new URL(personUrl.LongUrl);
+
+                return Json(url.ToString());
+            }
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UrlCutter/UrlCutter/Models/URL.cs
@@ -0,0 +1,67 @@
+﻿using System.IO.Hashing;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace UrlCutter.Models
+{
+    public class URL
+    {
+        public string? LongUrl { get; set; }
+        public string? ShortUrl { get; set; }
+        public string? Token { get; set; }
+
+        public URL() { }
+        public URL(string url)
+        {
+            if (IsUrl(url))
+            {
+                LongUrl = url;
+                Token = CreateToken(url);
+                ShortUrl = CreateTinyUrl(url, Token);
+            }
+            else
+            {
+                throw new Exception("Введенная строка не является url");
+            }
+        }
+
+        public bool IsUrl(string url)
+        {
+            Regex regex = new Regex("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$");
+
+            MatchCollection matches = regex.Matches(url);
+            if (matches.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string CreateToken(string longUrl)
+        {
+            Crc64 crc = new Crc64();
+            crc.Append(Encoding.ASCII.GetBytes(longUrl));
+            var token = Encoding.ASCII.GetString(crc.GetCurrentHash());
+            return token;
+        }
+
+        private string CreateTinyUrl(string longUrl, string token)
+        {
+            var split = longUrl.Split('/');
+
+            string domen = "";
+            for (int i = 0; i < 3; i++)
+            {
+                domen += split[i] + "/";
+            }
+
+            string tinyUrl = domen + token;
+            return tinyUrl;
+        }
+
+        public override string ToString()
+        {
+            return $"LongUrl: {LongUrl} \nShortUrl: {ShortUrl} \nToken: {Token}";
+        }
+    }
+}
